af554fb0a997301f8fa4e175ea235135dbb7ab26
Ali Madooei <alimadooei@gmail.com>
2023-09-05 11:25:54 -0400

    Initialize the State

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "../style.css";
+
+// Array to store our todos
+const todos = [
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Buy bread", completed: false },
+];
+let nextTodoId = 3;
+let filter = "all"; // can be 'all', 'active', or 'completed'
