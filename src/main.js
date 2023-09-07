186187fd1c7a0cada12e491efda971a312d687bb
Ali Madooei <alimadooei@gmail.com>
2023-09-07 11:23:06 -0400

    Render the Todos

--- src/main.js
+++ src/main.js
@@ -52,37 +52,27 @@ function renderTodos() {
   const todoListElement = document.getElementById('todo-list');
   todoListElement.innerHTML = ''; // clear the current list
 
-  let filteredTodos = [];
-  for (let i = 0; i < todos.length; i++) {
-    if (filter === 'all') {
-      filteredTodos.push(todos[i]);
-    } else if (filter === 'active' && !todos[i].completed) {
-      filteredTodos.push(todos[i]);
-    } else if (filter === 'completed' && todos[i].completed) {
-      filteredTodos.push(todos[i]);
-    }
-  }
+  const filteredTodos = todoApp.getVisibleTodos();
 
-  // Loop through the filtered todos and add them to the DOM
-  for (let i = 0; i < filteredTodos.length; i++) {
+  filteredTodos.forEach((todo) => {
     const todoItem = document.createElement('div');
     todoItem.classList.add('p-4', 'todo-item');
 
     const todoText = document.createElement('div');
     todoText.classList.add('todo-text');
-    todoText.textContent = filteredTodos[i].text;
-    if (filteredTodos[i].completed) {
+    todoText.textContent = todo.text;
+    if (todo.completed) {
       todoText.style.textDecoration = 'line-through';
     }
 
     const todoEdit = document.createElement('input');
     todoEdit.classList.add('hidden', 'todo-edit');
-    todoEdit.value = filteredTodos[i].text;
+    todoEdit.value = todo.text;
 
     todoItem.appendChild(todoText);
     todoItem.appendChild(todoEdit);
     todoListElement.appendChild(todoItem);
-  }
+  })
 }
 
 // Function to handle adding a new todo
