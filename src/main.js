303ad791e5e085a7ee4458c2ef25a7163da4da7b
Ali Madooei <alimadooei@gmail.com>
2023-09-12 18:42:13 -0400

    Define the Todo Class

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import "../style.css";
 
+class Todo {
+  static nextId = 1;
+
+  constructor(text) {
+    this.id = Todo.nextId++;
+    this.text = text;
+    this.completed = false;
+  }
+
+  toggle() {
+    this.completed = !this.completed;
+  }
+}
