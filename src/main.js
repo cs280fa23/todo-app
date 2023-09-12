d0e419e82c871f3de741f9620ada6a4a7669c79a
Ali Madooei <alimadooei@gmail.com>
2023-09-12 18:46:59 -0400

    Define the TodoList Class

--- src/main.js
+++ src/main.js
@@ -19,3 +19,33 @@ const FILTERS = {
   ACTIVE: "active",
   COMPLETED: "completed",
 };
+
+class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(text) {
+    const todo = new Todo(text);
+    this.todos.push(todo);
+  }
+
+  // pre: todos' text are unique
+  toggleTodo(todoText) {
+    const todo = this.todos.find((t) => t.text === todoText);
+    if (todo) {
+      todo.toggle();
+    }
+  }
+
+  getVisibleTodos(filter) {
+    switch (filter) {
+      case FILTERS.ACTIVE:
+        return this.todos.filter((todo) => !todo.completed);
+      case FILTERS.COMPLETED:
+        return this.todos.filter((todo) => todo.completed);
+      default:
+        return this.todos;
+    }
+  }
+}
